c2db17b737cc84c004d60f5adf887c98b6acfb30
Chirag <chirag657krsingh@gmail.com>
2026-02-16 16:59:37 +0530

    Production configuration ready

--- backend/t20_backend/settings.py
+++ backend/t20_backend/settings.py
@@ -28,12 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&ibpl1(e-st=x_*3*8s87m3(go+m%b$a8klw^p&g41vlhe=i#o'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
